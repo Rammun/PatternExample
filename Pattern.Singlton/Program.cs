8fa7c1958a379ef7bbd9fb7faa3182c964cca373
Ruslan Likhobaba <likh.ruslan@gmail.com>
2016-08-03 18:10:51 +0300

    [add] Добавил реализацию синглтона

--- Pattern.Singlton/Program.cs
+++ Pattern.Singlton/Program.cs
@@ -10,6 +10,62 @@ namespace Pattern.Singlton
     {
         static void Main(string[] args)
         {
+            var ob1 = Singleton1.GetInstance();
+            ob1.Data = "Word1";
+            Console.WriteLine("ob1 - " + ob1.Data);
+
+            var ob2 = Singleton1.GetInstance();
+            Console.WriteLine("ob2 - " + ob2.Data);
+
+            ob1.Data = "Word2";
+            Console.WriteLine("ob2 - " + ob2.Data);
+
+            Console.ReadKey();
         }
+    }
+
+    public class Singleton1
+    {
+        private static Singleton1 instance;
+        private string data = string.Empty;
+
+        private Singleton1()
+        { }
+
+        public static Singleton1 GetInstance()
+        {
+            if (instance == null)
+                instance = new Singleton1();
+
+            return instance;
+        }
+
+        public string Data { get; set; }
+    }
+
+    public class Singleton2
+    {
+        static Singleton2 instance = null;
+        static readonly object block = new object();
+
+        private string data = string.Empty;
+
+        private Singleton2()
+        {
+        }
+
+        public static Singleton2 GetInstance()
+        {
+            lock (block)
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton2();
+                }
+                return instance;
+            }
+        }
+
+        public string Data { get; set; }
     }
 }
